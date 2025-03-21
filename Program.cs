d15527ca5245a95618005f17e03ac913c24a3464
Miguel <miguel.perassoli@gmail.com>
2025-03-21 17:43:17 -0300

    cebolinha

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Clear();
+Console.WriteLine("Escreva cebolinha");
+string x = Console.ReadLine()!.ToUpper();
+string y = x 
+       .Replace("R","L")
+;
+
+Console.WriteLine($@"Vc escreveu isso {x} mas Cebolinha falaria assim {y}");
